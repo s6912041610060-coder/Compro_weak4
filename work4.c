b3e80f4efc27becf1a3a5b2a0ac655d15ae1f798
s6912041610060-coder <s6912041610060@email.kmutnb.ac.th>
2026-07-24 09:24:43 +0000

    home work

---
+++ work4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    char name_first;
+    int age;
+
+
+    printf("Enter the first character of your name: ");
+    scanf(" %c", &name_first);
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    
+    printf("Hi %c. You are %d years old. \n", name_first, age);
+
+    return 0;
+}
